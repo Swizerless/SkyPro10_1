13c1465bfe6ca4739d6de6e8af30343b8a5acf84
Swizerless <swizerless@gmail.com>
2025-05-25 13:26:20 +0100

    Прогнал линтерами widget.py и исправил все недочёты

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,12 @@
-from typing import Union
 import re
-from src.masks import get_mask_card_number, get_mask_account
+from typing import Any
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(bank_details: Union[str, int]) -> Union[str, int]:
-    """Функция принимает один аргумент — строку, содержащую тип и номер карты или счета, а возвращает строку с замаскированным номером"""
+def mask_account_card(bank_details: Any[str, int]) -> Any[str, int]:
+    """Функция принимает один аргумент — строку, содержащую тип и номер карты или счета,
+    а возвращает строку с замаскированным номером"""
     split_parts = bank_details.split()
     if "Счет" in bank_details or "Счёт" in bank_details:
         mask_account_number = get_mask_account(split_parts[-1])
@@ -17,9 +19,11 @@ def mask_account_card(bank_details: Union[str, int]) -> Union[str, int]:
         card_result = f"{card_type} {mask_card_number}"
         return card_result
 
+
 def get_date(date: str) -> str:
-    """Функция принимает на вход строку с датой в странном формате и возвращает строку с датой в формате (ДД.ММ.ГГГГ)"""
-    split_date = re.split('[-T]', date)
+    """Функция принимает на вход строку с датой в странном формате
+    и возвращает строку с датой в формате (ДД.ММ.ГГГГ)"""
+    split_date = re.split("[-T]", date)
     # нашёл инфу по сплиту и регулярным выражениям на сайте Skypro, классная вещь!
     date_list = [split_date[2], split_date[1], split_date[0]]
     date_result = ".".join(date_list)
