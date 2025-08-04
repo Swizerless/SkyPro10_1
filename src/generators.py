fb5b7b12e1aaee7c0b543647b26af8cf471a5ba1
Swizerless <swizerless@gmail.com>
2025-08-04 13:58:41 +0100

    Дописал функцию transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -10,3 +10,8 @@ def filter_by_currency(transactions_list: list, currency_code: str) -> Iterator:
             lambda x: x.get("operationAmount", {}).get("currency", {}).get("code") == currency_code, transactions_list
         )
     )
+
+def transaction_descriptions(transactions_list: list[dict]) -> Iterator:
+    """пример реализации генератора"""
+    for t in transactions_list:
+        yield t["description"]
